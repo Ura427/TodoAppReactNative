7795987cd67684122658fddc8965193728691b38
Ura427 <urakost427@gmail.com>
2024-03-10 21:45:14 +0200

    rebuild for using new components

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, Modal } from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItems from "./components/GoalItems";
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [goals, setGoals] = useState([]);
+
+  const deleteGoalHandler = (id) => {
+    setGoals(goals.filter((goal) => goal.id !== id));
+  };
+
+  const modalCloseHandler = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <Modal
+        visible={modalVisible}
+        style={styles.modalStyles}
+        animationType="fade"
+        transparent={false}
+      >
+        <GoalInput setGoals={setGoals} modalCloseHandler={modalCloseHandler} />
+      </Modal>
+      <Button
+        title="Add Goal"
+        onPress={() => setModalVisible((prev) => !prev)}
+      />
+
+      <GoalItems goals={goals} deleteGoalHandler={deleteGoalHandler} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 80,
+    paddingHorizontal: 40,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#222831",
+  },
+
+  modalStyles: {
+    flex: 1,
+    backgroundColor: "white",
   },
 });
