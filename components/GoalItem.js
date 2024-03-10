14be52ee25efa9979afad0ad663aadca2f0de600
Ura427 <urakost427@gmail.com>
2024-03-10 21:13:27 +0200

    create component for goal item

---
+++ components/GoalItem.js
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+function GoalItem({ index, item, dueDate, deleteGoalHandler }) {
+  const hours = dueDate.getHours();
+  const minutes = dueDate.getMinutes();
+  
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  
+  const formattedTime = `${hours}:${formattedMinutes}`;
+  
+
+  return (
+    <View key={index} style={styles.goalContainer}>
+      <Pressable
+        android_ripple={{ color: "#018187" }}
+        onPress={deleteGoalHandler}
+      >
+        <View style={styles.textsContainer}>
+          <Text style={styles.goalText}>{item}</Text>
+          <Text style={styles.goalDueDate}>{formattedTime}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  goalText: {
+    color: "#fff",
+    fontWeight: "bold"
+    // padding: 8,
+  },
+  goalContainer: {
+    backgroundColor: "#00ADB5",
+    marginBottom: 8,
+    borderRadius: 4,
+    // flexDirection: "row",
+    // justifyContent: "space-between"
+  },
+  textsContainer:{
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 8
+  }, 
+  goalDueDate:{
+   color: "#eee"
+  }
+});
+
+export default GoalItem;
